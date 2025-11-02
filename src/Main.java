7c22daa8fcc1f1d411a845f2e4a45d0734e12042
berkee25 <berkeozdemir25@gmail.com>
2025-11-02 18:11:44 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,12 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
-
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+public class Main {
+    public static void main(String[] args) {
+        myQueue myqueue = new myQueue();
+        myqueue.enqueue(10);
+        myqueue.enqueue(20);
+        myqueue.enqueue(30);
+        myqueue.enqueue(40);
+        myqueue.print(); //after the queue method 10 will not be visible in the output
+        myqueue.dequeue();//first in first out
+        myqueue.print();
+    }
 }
