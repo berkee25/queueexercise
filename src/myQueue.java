8b91618aacd57be16cc816d42460752199e61c84
berkee25 <berkeozdemir25@gmail.com>
2025-11-02 18:11:19 +0300

    Update myQueue.java

--- src/myQueue.java
+++ src/myQueue.java
@@ -1,4 +1,52 @@
-package PACKAGE_NAME;
-
+class Node{
+    int data;
+    Node next;
+    Node(int data){
+        this.data=data;
+        this.next=null;
+    }
+}
 public class myQueue {
+    Node front;
+    Node rear;
+
+    myQueue(){
+        front = rear=null;
+    }
+    public void enqueue(int data){
+        Node newNode=new Node(data);
+        if(front==null&& rear == null){
+            front = rear=newNode;
+            return;
+        }
+        if(rear==null){
+            front = rear=newNode;
+            return;
+        }
+        rear.next=newNode;
+        rear=newNode;
+    }
+    public int dequeue(){
+        if (front == null) {
+            System.out.println("Queue is empty ");
+            return -1;
+        }
+        int value = front.data;
+        front = front.next;
+        if (front == null) {
+            rear = null;
+        }
+        return value;
+    }
+    public void print() {
+        Node temp = front;
+        while (temp != null) {
+            System.out.print(temp.data);
+            if(temp.next!=null){
+                System.out.print("->");
+            }
+            temp = temp.next;
+        }
+        System.out.println();
+    }
 }
